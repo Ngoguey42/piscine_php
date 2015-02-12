fb274be83f9c50e62b52cb753ed08af8863002aa
Ngoguey42 <ngoguey@student.42.fr>
2015-02-12 07:20:56 +0100

    finished j02 ex03

--- j02_PHP_2/ex03/who.php
+++ j02_PHP_2/ex03/who.php
@@ -1,27 +1,21 @@
 #!/usr/bin/php
 <?php
-$tab = scandir("/dev", SCANDIR_SORT_NONE);
-/* print_r($tab); */
-$myuid = posix_getuid();
+$file = fopen("/var/run/utmpx", "r");
 $myuid_s = get_current_user();
+fread($file, 1256);
 date_default_timezone_set("Europe/Paris");
-
-foreach ($tab as $v)
+$tab_results = array();
+while ($line = fread($file, 628))
 {
-	$stats = lstat("/dev/$v");
-	/* print($stats[6].filetype("/dev/$v")."\n"); */
-	if ($stats[4] != $myuid || filetype("/dev/$v") != "char")
-		continue ;
-	printf("%-8s %-8s date\n", $myuid_s, $v);
-	/* print("/dev/$v"); */
-	print(date("M j H:i\n", $stats[8]));
-	print(date("M j H:i\n", $stats[9]));
-	print(date("M j H:i\n", $stats[10]));
-	print(date("M j H:i\n", filemtime("/dev/$v")));
-	print(date("M j H:i\n", fileatime("/dev/$v")));
-	print(date("M j H:i\n", filectime("/dev/$v")));
-	/* print_r($stats); */
-	/* print("\n"); */
+	$tab = unpack("a256login/a4id/a32line/ipid/itype/itime/a256host/I16pad", $line);
+	if ($tab["login"] == $myuid_s)
+		$tab_results[$tab["line"]] = $tab["time"];
 }
-
+ksort($tab_results);
+foreach($tab_results as $k => $v)
+{
+	printf("%-8s %-8s %s\n", $myuid_s, $k, date("M j H:i", $v));	
+	
+}
+fclose($file);
 ?>
