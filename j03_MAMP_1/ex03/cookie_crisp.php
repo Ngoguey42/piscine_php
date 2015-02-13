2953691bd64e612e41bed1ee6293bf2ea429e013
Ngoguey42 <ngoguey@student.42.fr>
2015-02-13 13:07:04 +0100

    finished ex03... i think

--- j03_MAMP_1/ex03/cookie_crisp.php
+++ j03_MAMP_1/ex03/cookie_crisp.php
@@ -3,10 +3,10 @@ if ($_GET['name'] != null)
 {
 	if ($_GET["action"] === "set" && $_GET['value'] != null)
 		setcookie($_GET['name'], $_GET['value'], time() + 3600 * 24 * 30);
-	elseif ($_GET['action'] === "get")
+	elseif ($_GET['action'] === "get" && $_COOKIE[$_GET['name']])
 	{
 		$val = $_GET['name'];
-		echo "$_COOKIE[$val]\n";
+		echo $_COOKIE[$_GET['name']]."\n";
 	}
 	elseif ($_GET['action'] === "del")
 		setcookie($_GET['name'], "");
--- j03_MAMP_1/ex03/test.sh
+++ j03_MAMP_1/ex03/test.sh
@@ -2,24 +2,49 @@
 
 function catcook()
 {
-	echo "cat cook.txt:\033[31m"
+	echo "\033[0mcat cook.txt:\033[31m"
 	cat cook.txt
 	echo "\033[0m\n"
 }
 
+function curlit()
+{
+	echo "curl" $1 cook.txt ...$2 ":\033[32m"
+	curl $1 cook.txt $url$2
+
+	
+	catcook
+}
+
+
+
 url='http://j03.local.42.fr:8080/ex03/cookie_crisp.php'
+clear
 
 rm cook.txt
 catcook
 
+curlit -c '?action=set&name=plat&value=choucroute'
+curlit -b '?action=get&name=plat'
+curlit -c '?action=del&name=plat'
+curlit -b '?action=get&name=plat'
 
 
-curl -c cook.txt $url'?action=set&name=plat&value=choucroute'
-catcook
 
-curl -c cook.txt $url'?action=set&name=plat2&value=choucroute2'
-catcook
+# curl -c cook.txt $url'?action=set&name=plat&value=choucroute'
+# catcook
 
-curl -c cook.txt $url'?action=del&name=plat'
-catcook
+# curl -c cook.txt $url'?action=set&name=plat2&value=choucroute2'
+# catcook
 
+
+# curl -b cook.txt $url'?action=get&name=plat'
+# catcook
+
+
+# curl -c cook.txt $url'?action=del&name=plat'
+# catcook
+
+
+# curl -b cook.txt $url'?action=get&name=plat'
+# catcook
