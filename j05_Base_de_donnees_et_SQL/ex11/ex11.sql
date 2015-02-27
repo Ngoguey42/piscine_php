4b0d951915260fb33194161033abf74c76270a19
Ngoguey42 <ngoguey@student.42.fr>
2015-02-27 08:37:33 +0100

    ex6 to ex11

---
+++ j05_Base_de_donnees_et_SQL/ex11/ex11.sql
@@ -0,0 +1,6 @@
+SELECT UPPER(fiche.`nom`) AS `NOM`, fiche.`prenom`, abo.`prix`
+	   FROM `fiche_personne` fiche
+	   INNER JOIN `membre` memb ON fiche.`id_perso` = memb.`id_fiche_perso`
+	   INNER JOIN `abonnement` abo ON memb.`id_abo` = abo.`id_abo`
+	   WHERE abo.`prix` > 42
+	   ORDER BY fiche.`nom` ASC, fiche.`prenom` ASC;
