8d40007af60362586ae846204029e25db34e1411
Ngoguey42 <ngoguey@student.42.fr>
2015-04-07 10:07:06 +0200

    ex0 1 2

--- j01_PHP_1/ex02/oddeven.php
+++ j01_PHP_1/ex02/oddeven.php
@@ -1,25 +1,24 @@
 #!/usr/bin/php
-<?php
-$stdin = new SplFileObject("php://stdin");
+<?PHP
 while (1)
 {
 	echo "Entrez un nombre: ";
-	$line = $stdin->current();
-	if ($stdin->eof())
+	$line = fgets(STDIN);
+	if (feof(STDIN))
+		exit(PHP_EOL);
+	if (preg_match("/^\s*".
+		"([\+\-]?)([0-9]*)([0-9])".
+		"\n?$/", $line, $tab))
 	{
-		echo "\n";
-		break ;
-	}
-	$line = trim($line);
-	if (preg_match("/^[\+\-]?([0-9]+)$/", $line, $tab))
-	{
-		if ($tab[1] % 2)
-			echo "Le chiffre $line est Impair\n";
+		if ($tab[3] % 2)
+			echo "Le chiffre $tab[1]$tab[2]$tab[3] est Impair".PHP_EOL;
 		else
-			echo "Le chiffre $line est Pair\n";		
+			echo "Le chiffre $tab[1]$tab[2]$tab[3] est Pair".PHP_EOL;
 	}
 	else
-		echo "'$line' n'est pas un chiffre\n";
-	$stdin->next();
+	{
+		$line = substr($line, 0, -1);
+		echo "'$line' n'est pas un chiffre".PHP_EOL;
+	}
 }
 ?>
